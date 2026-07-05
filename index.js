ce045994803c79c8576f2cf2cb29a423c0aa3bca
gigagame4031 <gigagame4031@gmail.com>
2026-07-05 22:17:56 +0330

    index.js

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ client.once("ready", () => {
   console.log("Bot is online!");
 });
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+app.listen(3000);
+
 client.login(process.env.TOKEN);
